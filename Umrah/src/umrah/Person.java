9cd568090f3600871103c6c477fc8e06290b32bc
Sadeen-Althagafi <90712400+Sadeen-Althagafi@users.noreply.github.com>
2021-12-01 13:14:34 +0300

    Update Person.java

--- Umrah/src/umrah/Person.java
+++ Umrah/src/umrah/Person.java
@@ -16,5 +16,60 @@ public class Person {
     String Password;
     long PhoneNum;
     long National_ID;
-    
+    public Person(String Fname, String Lname, String Email, String Password, long PhoneNum, long National_ID) {
+        this.Fname = Fname;
+        this.Lname = Lname;
+        this.Email = Email;
+        this.Password = Password;
+        this.PhoneNum = PhoneNum;
+        this.National_ID = National_ID;
+    }
+
+    public void setFname(String Fname) {
+        this.Fname = Fname;
+    }
+
+    public void setLname(String Lname) {
+        this.Lname = Lname;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+
+    public void setPhoneNum(long PhoneNum) {
+        this.PhoneNum = PhoneNum;
+    }
+
+    public void setNational_ID(long National_ID) {
+        this.National_ID = National_ID;
+    }
+
+    public String getFname() {
+        return Fname;
+    }
+
+    public String getLname() {
+        return Lname;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public long getPhoneNum() {
+        return PhoneNum;
+    }
+
+    public long getNational_ID() {
+        return National_ID;
+    }}
 }
